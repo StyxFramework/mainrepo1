ae772f5baa0032ad4104c79781b85b1435f07719
Srivatsan <ssrivatsan@athenahealth.com>
2017-08-17 16:01:15 +0530

    Added TestNG

--- Framework/styxframework/src/test/java/athn/framework/TestLogin.java
+++ Framework/styxframework/src/test/java/athn/framework/TestLogin.java
@@ -1,18 +1,30 @@
 package athn.framework;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import athn.styxframework.pages.LoginSignup;
 
 public class TestLogin {
+	LoginSignup objlogin = new LoginSignup();
+	//WebElement objUsername = objlogin.loginUsername;
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@BeforeTest
+	public void startChrome(){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssrivatsan\\Desktop\\chromedriver.exe");
-		 WebDriver driver = new ChromeDriver();
-		 driver.get("https://styxframework.github.io/mainrepo/");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://styxframework.github.io/mainrepo/");
+		
+	}
+	@Test
+	public void test_fill_login(){
+		
 	}
 
 }
