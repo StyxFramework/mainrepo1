0d5eb3db2985bf736f081ca17e97af0ad80f6e0d
Nivetha <pnivetha21@gmail.com>
2017-08-21 15:05:03 +0530

    Login assert

--- Framework/styxframework/src/main/java/athn/styxframework/pages/LoginSignup.java
+++ Framework/styxframework/src/main/java/athn/styxframework/pages/LoginSignup.java
@@ -12,9 +12,12 @@ public class LoginSignup {
  public LoginSignup(WebDriver driver) {
 	 this.driver=driver;
  }
- public void signin(String username, String password)
+ public String signin(String username, String password)
  {
 	 driver.findElement(loginUsername).sendKeys(username);
 	 driver.findElement(loginPassword).sendKeys(password);
+	 driver.findElement(loginBtn).click();
+	 String alertMessage = driver.switchTo().alert().getText();
+	 return alertMessage;
  }
 }
--- Framework/styxframework/src/test/java/athn/framework/TestLogin.java
+++ Framework/styxframework/src/test/java/athn/framework/TestLogin.java
@@ -1,9 +1,8 @@
 package athn.framework;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -21,12 +20,11 @@ public class TestLogin {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nprabakaran\\Documents\\GitHub\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://styxframework.github.io/mainrepo1/");
-		//driver.findElement(By.name("loginUsername")).sendKeys("admin");
 	}
 	@Test
 	public void test_fill_login(){
 		objlogin = new LoginSignup(driver);
-		objlogin.signin("admin", "admin");
+		String outputMessage = objlogin.signin("admin", "admin1");
+		Assert.assertEquals(outputMessage, "Succssfully Logged in!", "Login Failed");
 	}
-
 }
