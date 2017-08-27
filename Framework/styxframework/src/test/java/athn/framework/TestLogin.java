7f77d720b9b0afb20a003c87a324573a9f502ffb
Srivatsan <vatsan34@gmail.com>
2017-08-27 13:58:21 +0530

    Update TestLogin.java

--- Framework/styxframework/src/test/java/athn/framework/TestLogin.java
+++ Framework/styxframework/src/test/java/athn/framework/TestLogin.java
@@ -1,30 +1,30 @@
 package athn.framework;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import athn.styxframework.pages.LoginSignup;
 
 public class TestLogin {
-	LoginSignup objlogin = new LoginSignup();
-	//WebElement objUsername = objlogin.loginUsername;
+	WebDriver driver;
+	LoginSignup objlogin;
 
 	/**
 	 * @param args
 	 */
 	@BeforeTest
 	public void startChrome(){
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssrivatsan\\Desktop\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://styxframework.github.io/mainrepo/");
-		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nprabakaran\\Documents\\GitHub\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://styxframework.github.io/mainrepo1/");
 	}
 	@Test
 	public void test_fill_login(){
-		objlogin.signin("admin", "admin");
+		objlogin = new LoginSignup(driver);
+		String outputMessage = objlogin.signin("admin", "admin1");
+		Assert.assertEquals(outputMessage, "Succssfully Logged in!", "Login Failed");
 	}
-
 }
