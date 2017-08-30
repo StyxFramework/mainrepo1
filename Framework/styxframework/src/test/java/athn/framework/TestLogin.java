eea4e8a9b00fd4ae15318bd556082fc5ab2c27bb
Srivatsan <vatsan34@gmail.com>
2017-08-30 13:35:23 +0530

    Update TestLogin.java

--- Framework/styxframework/src/test/java/athn/framework/TestLogin.java
+++ Framework/styxframework/src/test/java/athn/framework/TestLogin.java
@@ -1,14 +1,11 @@
 package athn.framework;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -30,30 +27,44 @@ public class TestLogin {
 		driver.get("https://styxframework.github.io/mainrepo1/");
 	}
 	@Test(dataProvider = "TestCases")
-	public void test_fill_login(List<Integer> TestCase) throws IOException {
+	public void test_fill_login(String[] TestCase) throws IOException {
     	readFromExcel objExcelFile = new readFromExcel();
         String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\excelOps";
         Sheet getSheet = objExcelFile.getSheet(filePath,"Styx.xlsx","Data");
-        String loginUsername = objExcelFile.getValueFromSheet(getSheet,"login_username");
-        String loginPassword = objExcelFile.getValueFromSheet(getSheet,"login_password");
+        String loginUsername = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_username");
+        String loginPassword = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_password");
+        String loginMessage = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_message");
 		objlogin = new LoginSignup(driver);
 		String outputMessage = objlogin.signin(loginUsername, loginPassword);
-		Assert.assertEquals(outputMessage, "Succssfully Logged in!", "Login Failed");
-		
-		String signupresult = objlogin.signup("", "");
-		Assert.assertEquals(signupresult, "firstname and lastname are empty", "Signup alert passed");
+		Assert.assertEquals(outputMessage, loginMessage, "Success");
 		driver.switchTo().alert().accept();
+	}
+	
+	@Test(dataProvider = "TestCases")
+	public void test_fill_signup(String[] TestCase) throws IOException {
+    	readFromExcel objExcelFile = new readFromExcel();
+        String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\excelOps";
+        Sheet getSheet = objExcelFile.getSheet(filePath,"Styx.xlsx","Data");
+        String loginUsername = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_username");
+        String loginPassword = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_password");
+        String loginMessage = objExcelFile.getValueFromSheet(getSheet,TestCase[0],"login_message");
+		objlogin = new LoginSignup(driver);
+		String outputMessage = objlogin.signin(loginUsername, loginPassword);
+		Assert.assertEquals(outputMessage, loginMessage, "Success");
+		driver.switchTo().alert().accept();		
+	}
+	
+	@AfterTest
+	public void closeChrome(){
+	driver.quit();
 	}
 
 	@DataProvider
 	public Object[] TestCases() throws Exception{
-		String[] runnableTests = new String[8];
 		readFromExcel getTestCases = new readFromExcel();
 		String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\excelOps";
-		Sheet getTCSheet = getTestCases.getSheet(filePath,"Styx.xlsx","Test_Cases");
-		runnableTests = getTestCases.getRunnableTestCases(getTCSheet);
 		Sheet getSheet = getTestCases.getSheet(filePath,"Styx.xlsx","Data");
-		int rowCount = getTestCases.getRowCount(getSheet);
+		String[] runnableTests = getTestCases.getRunnableTestCases(getSheet);
 		return runnableTests;
 	}
 }
